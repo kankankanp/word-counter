985889dc3a045e82055218abc89c83b1ed51fd3d
southvillage <kan0825ta@gmail.com>
2025-01-26 01:40:22 +0900

    UI作成

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Page() {
         <h1 className="text-black text-2xl font-bold mb-4">文字数カウンター</h1>
         <textarea
           placeholder="ここにテキストを入力"
-          className="w-full h-32 p-4 border border-gray rounded-md focus:outline-black mb-6 text-black"
+          className="w-full h-[300px] p-4 border border-gray rounded-md focus:outline-black mb-6 text-black"
         ></textarea>
 
         <div className="flex justify-end gap-4 mb-6">
@@ -17,21 +17,21 @@ export default function Page() {
           <tbody>
             <tr>
               <th className="border-white border-b-[1px] px-4 py-2 text-white bg-lightBlue">文字数</th>
-              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-xl">172<span className="text-sm">文字</span></td>
+              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-2xl">172<span className="text-sm"> 文字</span></td>
               <th className="border-white border-b-[1px] px-4 py-2 text-white bg-lightBlue">空白数</th>
-              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-xl">172<span className="text-sm">文字</span></td>
+              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-2xl">172<span className="text-sm"> 文字</span></td>
             </tr>
             <tr>
               <th className="border-white border-b-[1px] px-4 py-2 text-white bg-lightBlue">英単語数</th>
-              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-xl">172<span className="text-sm">文字</span></td>
+              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-2xl">172<span className="text-sm"> 文字</span></td>
               <th className="border-white border-b-[1px] px-4 py-2 text-white bg-lightBlue">文字数(空白なし)</th>
-              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-xl">172<span className="text-sm">文字</span></td>
+              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-2xl">172<span className="text-sm"> 文字</span></td>
             </tr>
             <tr>
               <th className="border-white border-b-[1px] px-4 py-2 text-white bg-lightBlue">全ての文字数</th>
-              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-xl">172<span className="text-sm">文字</span></td>
+              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-2xl">172<span className="text-sm"> 文字</span></td>
               <th className="border-white border-b-[1px] px-4 py-2 text-white bg-lightBlue">文字数(空白あり)</th>
-              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-xl">172<span className="text-sm">文字</span></td>
+              <td className="border border-lightBlue px-4 py-2 text-black font-bold text-2xl">172<span className="text-sm"> 文字</span></td>
             </tr>
           </tbody>
         </table>
